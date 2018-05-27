5ee597c0aadbb2d227d82532712fd9c80c82d449
rommel_dt <rmmelpneda.myproject@gmail.com>
2018-05-27 13:29:25 +0300

    First Initial Commit of WebApi from Desktop

---
+++ WebAPI/Controllers/MallController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class MallController : ApiController
+    {
+        // GET api/mall
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/mall/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/mall
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/mall/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/mall/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPI/Controllers/MallsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class MallsController : ApiController
+    {
+        // GET api/mall
+        public IEnumerable<Mall> Get()
+        {
+            using (ARMAEntities armaentities = new ARMAEntities())
+            {
+                return armaentities.Malls.ToList();//this will return the list of item in db
+            }
+        }
+
+        // GET api/mall/{id}        
+        public HttpResponseMessage Get(int id)
+        {
+            using (ARMAEntities armaentities = new ARMAEntities())
+            {
+                var entity = armaentities.Malls.FirstOrDefault(e => e.id == id);
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with Id=" + id.ToString() + "Not Found");
+                }
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        //DELETE api/values/{id}
+        public void Delete(int id)
+        {
+        }
+
+        // POST api/values
+        public HttpResponseMessage Post([FromBody]Mall mall)
+        {
+            try
+            {
+                using (ARMAEntities armaentities = new ARMAEntities())
+                {
+                    armaentities.Malls.Add(mall);
+                    armaentities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, mall);
+                    message.Headers.Location = new Uri(Request.RequestUri + mall.id.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, ex.Message.ToString());
+            }
+        }
+
+
+    }
+}
